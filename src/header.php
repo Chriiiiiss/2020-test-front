3e96582838c7f3964d9d35c687e983256426d0fc
Chris <60599836+Chriiiiiss@users.noreply.github.com>
2021-07-02 13:09:42 +0200

    :rocket: [ADD] Finishing the header

--- src/header.php
+++ src/header.php
@@ -3,11 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="../src/css/normalize.css">
     <link rel="stylesheet" href="../src/css/header.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Punk API</title>
 </head>
 <body>
     <header>
-        <span>B</span>
+        <div class="header_container">
+            <span class="logo">B</span>
+            <span class="nice_beer">🍺</span>
+        </div>
     </header>
