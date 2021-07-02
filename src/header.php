59013ffbc3601971cb6787818e4df1cd31774b63
Chris <60599836+Chriiiiiss@users.noreply.github.com>
2021-07-02 12:52:44 +0200

    :rocket: [ADD] Normarlize.css

--- src/header.php
+++ src/header.php
@@ -3,8 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="../src/css/header.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Punk API</title>
 </head>
 <body>
-    <h1>Hello World</h1>
+    <header>
+        <span>B</span>
+    </header>
